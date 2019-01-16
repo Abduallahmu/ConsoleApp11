3f99c697330999112b6433db97d27898fe1807e6
deltagare <deltagare@LEXJONKAF8>
2019-01-16 14:50:09 +0100

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace typer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej det c# typer");
+
+            Console.WriteLine("int");
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+
+            Console.WriteLine("SByte");
+            Console.WriteLine(SByte.MinValue);
+            Console.WriteLine(SByte.MaxValue);
+
+            Console.WriteLine("long");
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            Console.WriteLine("ulong");
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(ulong.MaxValue);
+
+            Console.WriteLine("Shrot");
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(short.MaxValue);
+
+            Console.WriteLine("ushort");
+            Console.WriteLine(ushort.MinValue);
+            Console.WriteLine(ushort.MinValue);
+
+            Console.WriteLine("uint");
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(uint.MaxValue);
+
+            Console.WriteLine("float");
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+
+            Console.WriteLine("dobule");
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+
+            Console.WriteLine("char");
+            Console.WriteLine(char.MinValue);
+            Console.WriteLine(char.MaxValue);
+
+            Console.WriteLine("decimal");
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+
+
+            Console.WriteLine("hejdå");
+            Console.ReadKey();
+
+            //
+
+        }
+    }
+}
